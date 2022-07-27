47112f0c4c4eb2efc9b686886942518266b3756b
Wellington Quadros <wellingtonquadros91@outlook.com>
2022-07-26 21:25:04 -0300

    Atividade react1

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+function Home(){
+  return (
+    <>
+      <h1 className= "titulo">Home</h1>
+      <img src="https://i.imgur.com/83Xlpzy.jpg" alt="Simbolo do Clube de futebol Corinthians" className = "img"/>
+    </>
+  );
+}
+
+export default Home;
